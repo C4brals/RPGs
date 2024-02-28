f5300ee2c07899f3555bf96479c033969e748678
C4brals <augustoc4bral@gmail.com>
2024-02-28 14:22:36 -0300

    Atualização de estudos

---
+++ string/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+var texto = " Este texto é um teste ";
+
+//Console.WriteLine(texto.ToUpper());
+//Console.WriteLine(texto.ToLower());
+//Console.WriteLine(texto.Insert(5, "Aqui ")); //Insere algum texto a partir da posição 5.
+//Console.WriteLine(texto.Remove(5, 5)); //A partir da posição 5, remove os próximos 5 caracteres.
+//Console.WriteLine(texto.Length()); //Contabiliza a quantidade de caracteres de uma String.
+//Console.WriteLine(texto.Replace("Este", "Esse"));Substitui todo texto "Este", por "Esse".
+
+/*var divisao = texto.Split(" "); //Quebra de linha sempre que houver um espaço " "
+Console.WriteLine(divisao[0]);
+Console.WriteLine(divisao[1]);
+Console.WriteLine(divisao[2]);
+Console.WriteLine(divisao[3]);
+Console.WriteLine(divisao[4]);
+
+var resultado = texto.Substring(5, 4);
+
+Console.WriteLine(resultado);*/
+
+Console.WriteLine(texto.Trim());
